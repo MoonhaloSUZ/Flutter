21ffe70921d27dbf3c8f65b44377a9a659e82230
vvdbal@naver.com <vvdbal@naver.com>
2023-01-15 15:36:37 +0900

    null safety(변수?)

--- dart_for_flutter/bin/dart_null_safety.dart
+++ dart_for_flutter/bin/dart_null_safety.dart
@@ -2,6 +2,18 @@ import 'dart:js_util';
 
 void main() {
   //null safety : 개발자가 null값을 참조할 수 없도록 하는 기능
+String? sj = 'SJ'; //
+sj = null;
 
-  
+if(sj != null){
+  sj.isNotEmpty; 
+}
+//null safety란 어떤 변수 혹은 데이터가 null이 될 수 있음을 명시
+//swift에서도 구경해봤던 것 "변수?" 
+
+//위 조건문을 단축화하면 아래로 작성할 수 있다
+sj?.isNotEmpty;
+//sj가 낫 널이면 ~ isNotEmpty 속성을 달라 == 위 if 문과 동일
+
+
 }
