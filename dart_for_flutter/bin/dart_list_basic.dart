91ed8bf96e5543ca2aa6efa8be62bf34078d6b08
vvdbal@naver.com <vvdbal@naver.com>
2023-01-17 00:40:52 +0900

    리스트_collection for

---
+++ dart_for_flutter/bin/dart_list_basic.dart
@@ -0,0 +1,18 @@
+void main() {
+  //collection for
+
+  var oldFriends = ['sj', 'jk'];
+  var newFreinds = [
+    'a',
+    'b',
+    'c',
+    for(var friend in oldFriends) "♡ $friend",
+
+  ];
+
+  // for(var friend in oldFriends){
+  //   newFreinds.add()~
+  // }
+
+  print(newFreinds);
+}
