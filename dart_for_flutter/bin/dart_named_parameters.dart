d431ae2b0281fa2e85e5b54af8e53b4bb49edca3
vvdbal@naver.com <vvdbal@naver.com>
2023-01-24 23:59:42 +0900

    named parameter

---
+++ dart_for_flutter/bin/dart_named_parameters.dart
@@ -0,0 +1,20 @@
+String sayHello({String name, int age, String country}){
+  return "Hello $name, you are $age, and you come from $conuntry";
+
+}
+
+void main() {
+  print(sayHello('sj', 26, 'Korea'));
+
+  //named argument를 사용하면
+
+  print(sayHello(
+    age: 5,
+    country: 'new',
+    name: 'sj',
+  )); //함수의 인자에 중괄호해야 오류발생하지 않고 제대로 작동
+  //위 경우 유저가 모든 인자값을 넣지 않을 경우를 대비
+  //1. argument에 defalt 값 지정
+  //ex. String name = '123', int age = 99, String country = '123'
+
+}
