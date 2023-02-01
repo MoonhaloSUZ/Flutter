059ae8c86baf6e57e49d50b5da85d1202ace33ab
MoonhaloSUZ <vvdbal@naver.com>
2023-02-01 16:31:43 +0900

    QQ Operator and QQ Equals

--- dart_for_flutter/bin/dart_qqoperator.dart
+++ dart_for_flutter/bin/dart_qqoperator.dart
@@ -1,5 +1,3 @@
-import 'package:test/test.dart';
-
 String capital(String? name) {
   if (name != null) {
     return name.toUpperCase();
@@ -7,12 +5,32 @@ String capital(String? name) {
   return 'ANY';
 }
 
-String nocap(String? name) => name != null ? name.toLowerCase() : 'ANY';
+String nocap(String? name) => name != null ? name.toUpperCase() : 'ANY';
 
-String yescap(String? name) => name.toUpperCase() ?? 'ANY';
+String yescap(String? name) => name?.toUpperCase() ?? 'ANY';
 
 void main() {
   //이름을 대문자로 리턴하는 함수
-  capital('sj');
-  capital(null);
+  print(capital('sj'));
+  print(capital(null));
+  print(nocap('mm'));
+  print(yescap('lux'));
+
+  String? sj;
+  sj ??= 'sujin'; //sj가 null이면 'sujin' 할당
+  sj = null; //이 줄이 없으면 null 이 아니기 때문에 밑에서 에러 발생
+  //이미 sujin이라는 값이 들어와서 null이 아니기 때문
+  sj ??= 'asdf';
+  //위에서 null로 sujin 인입, 다시 null 초기화하여 null이기 대문에 최종적으로 asdf
+
+  print(sj);
 }
+
+//?? QQ Operator
+//좌항이 null이면 우항을 return
+//좌항이 null이 아니면 좌항을 return 
+
+//QQ equals(QQ assignment operator)
+//변수 ??= '변수';
+//변수가 널이면 '변수' 삽입
+
