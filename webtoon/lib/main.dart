33dc9a730822143ba83529f5d3b60c758bdd5690
MoonhaloSUZ <vvdbal@naver.com>
2023-03-05 16:57:57 +0900

    Container 그리기

--- webtoon/lib/main.dart
+++ webtoon/lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
 //runApp 호출 가능
 //void 함수인데 Widget이 필요
@@ -32,6 +34,7 @@ class App extends StatelessWidget{
         body: Padding(
           padding: EdgeInsets.symmetric(horizontal: 18 ),
           child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             SizedBox(
               height: 80,
@@ -59,7 +62,55 @@ class App extends StatelessWidget{
                   ],
                 )
               ],
-            )
+            ),
+            SizedBox(
+              height: 120,
+            ),
+            Text(
+              'Total Balance',
+              style: TextStyle(
+                fontSize: 22,
+                color: Colors.white.withOpacity(0.8),
+              ),
+              ),
+              SizedBox(
+                height: 10,
+                ),
+              Text(
+              '\$5 194 482',
+              //Dart에서는 텍스트 안에 변수를 넣을 때 $를 사용함
+              //따라서 변수가 아니도록 알려주기 위해 \사용
+              style: TextStyle(
+                fontSize: 44,
+                fontWeight: FontWeight.w600,
+                color: Colors.white,
+              ),
+              ),
+              SizedBox(
+                height: 30,
+                ),
+              Row(
+                children: [
+                  Container(
+                    decoration: BoxDecoration(
+                      color: Colors.amber,
+                      borderRadius: BorderRadius.circular(45),
+                    ),
+                    child: Padding(
+                      padding: EdgeInsets.symmetric(
+                        vertical: 20, 
+                        horizontal: 50
+                      ),
+                      child: Text('Transfer',
+                        style: TextStyle(
+                          fontSize: 20,
+                        ),
+                      ),
+                    ),
+
+                  )
+                ],
+                )
           ],
         ),
         )
