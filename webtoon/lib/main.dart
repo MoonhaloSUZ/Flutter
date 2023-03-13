40f941736b00b65d4b4e79fd5cb96c4b982be430
MoonhaloSUZ <vvdbal@naver.com>
2023-03-13 19:43:28 +0900

    BuildContext의 역할

--- webtoon/lib/main.dart
+++ webtoon/lib/main.dart
@@ -14,42 +14,46 @@ class App extends StatefulWidget {
 }
 
 class _AppState extends State<App> {
-  List<int> numbers = [];
-
-  void onClicked() {
-    setState(() {
-      numbers.add(numbers.length);
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+        theme: ThemeData(
+          textTheme: const TextTheme(
+            titleLarge: TextStyle(
+              color: Colors.red,
+            ),
+          ),
+        ),
         home: Scaffold(
             backgroundColor: Colors.white,
             body: Center(
               child: Column(
                   mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    const Text(
-                      'Click Count',
-                      style: TextStyle(
-                        fontSize: 30,
-                      ),
-                    ),
-                    for (var n in numbers)
-                      Text(
-                        '$n',
-                        style: const TextStyle(
-                          fontSize: 30,
-                        ),
-                      ),
-                    IconButton(
-                      iconSize: 40,
-                      onPressed: onClicked,
-                      icon: const Icon(Icons.add_box_rounded),
-                    )
+                  children: const [
+                    MyLargeTitle(),
                   ]),
             )));
+  }
+}
+
+class MyLargeTitle extends StatelessWidget {
+  const MyLargeTitle({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      'My Large Title',
+      style: TextStyle(
+        fontSize: 30,
+        color: Theme.of(context).textTheme.titleLarge?.color,
+        //theme의
+        //BuildContext 는 위젯 트리에서 위젯의 위치를 제공하고, 이를 통해 상위 요소 데이터에 접근할 수 있음
+        //단, 위 코드는 해당 요소가 존재하는지 알 수 없기 때문에(Dart는 null safety 언어)
+        //titleLarge? 혹은 titleLarge! 로 알려줘야함
+        //( 있거나?   /  무조건 있어요! )
+      ),
+    );
   }
 }
