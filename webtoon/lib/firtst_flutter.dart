3fc753e9f54316dc07b98364582d908e7058b006
MoonhaloSUZ <vvdbal@naver.com>
2023-03-18 22:38:03 +0900

    Flutter Start

--- webtoon/lib/firtst_flutter.dart
+++ webtoon/lib/firtst_flutter.dart
@@ -10,34 +10,36 @@ class Player {
 }
 
 void main() {
-  var dal = Player(name: 'dal',
+  var dal = Player(
+    name: 'dal',
   );
-  runApp(App());
-  
+  runApp(const App());
 }
 
-class App extends StatelessWidget{ 
+class App extends StatelessWidget {
+  const App({super.key});
+
 //StatelessWidget을 상속받음으로써 Widget이 됨
 //StatelessWidget.build 메소드를 반드시 사용하여야 함 -> build 메소드 : Widget의 UI를 만드는 것
 
-  @override 
+  @override
 //@overide : 부모 class에 이미 있는 메소드를 override 함
   Widget build(BuildContext context) {
-    return MaterialApp( //구글 형식의 material app일지 애플 형식의 cupertino app일지 정해야함. 구글에서 만든 언어로 material 이 더 고오급. 사용.
-      home: Scaffold( //Home 안에 다른 위젯을 생성
+    return MaterialApp(
+      //구글 형식의 material app일지 애플 형식의 cupertino app일지 정해야함. 구글에서 만든 언어로 material 이 더 고오급. 사용.
+      home: Scaffold(
+        //Home 안에 다른 위젯을 생성
         appBar: AppBar(
           centerTitle: false,
           elevation: 100,
-          title: Text('Hi, Flutter'),
-
+          title: const Text('Hi, Flutter'),
         ),
-        body: Center(
+        body: const Center(
           child: Text('Hi, Dal'),
-          ),
+        ),
       ),
     );
   }
 
   //material : 구글, cupertino : 애플
-  
 }
