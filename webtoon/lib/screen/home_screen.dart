e126050905e14a4e200ddae2a5fb80147c7c41f6
MoonhaloSUZ <vvdbal@naver.com>
2023-03-23 22:26:33 +0900

    button에 Timer (주기 : -1) 적용

--- webtoon/lib/screen/home_screen.dart
+++ webtoon/lib/screen/home_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -8,6 +10,23 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  int totalSeconds = 1500;
+  late Timer timer;
+  //사용자가 타이머를 누를 때만 활성화 시키고 싶어서 late
+  //late modifer는 해당 property를 당장 초기화하지 않아도 된다는 뜻
+  //단, 반드시 사용하기 전에 초기화해야 함.
+
+  void onTick(Timer timer) {
+    setState(() {
+      totalSeconds -= 1;
+    });
+  }
+
+  void onStartPressed() {
+    timer = Timer.periodic(const Duration(seconds: 1), onTick);
+    //onTick() 은 함수를 지금 당장 실행하는 것, 따라서 괄호를 제거하고 작성
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,7 +38,7 @@ class _HomeScreenState extends State<HomeScreen> {
           child: Container(
             alignment: Alignment.bottomCenter,
             child: Text(
-              '25:00',
+              '$totalSeconds',
               style: TextStyle(
                 color: Theme.of(context).cardColor,
                 fontSize: 89,
@@ -33,7 +52,7 @@ class _HomeScreenState extends State<HomeScreen> {
           child: Center(
               child: IconButton(
             //onPressed : 버튼을 눌렀을 때 실행되는 함수
-            onPressed: () {},
+            onPressed: onStartPressed,
             iconSize: 123,
             color: Theme.of(context).cardColor,
             icon: const Icon(
@@ -48,8 +67,8 @@ class _HomeScreenState extends State<HomeScreen> {
               Expanded(
                 child: Container(
                   decoration: BoxDecoration(
-                    color: Theme.of(context).cardColor,
-                  ),
+                      color: Theme.of(context).cardColor,
+                      borderRadius: BorderRadius.circular(50)),
                   child: Column(
                       mainAxisAlignment: MainAxisAlignment.center,
                       children: [
