1104cd072431b3b9fe81fab532fdab0a2019ea76
MoonhaloSUZ <vvdbal@naver.com>
2023-03-26 20:20:44 +0900

    Duration을 활용한 Formatting 방법

--- webtoon/lib/screen/home_screen.dart
+++ webtoon/lib/screen/home_screen.dart
@@ -10,7 +10,8 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  int totalSeconds = 1500;
+  static const tF = 1500;
+  int totalSeconds = tF;
   late Timer timer;
   //사용자가 타이머를 누를 때만 활성화 시키고 싶어서 late
   //late modifer는 해당 property를 당장 초기화하지 않아도 된다는 뜻
@@ -18,10 +19,21 @@ class _HomeScreenState extends State<HomeScreen> {
 
   bool isRunning = false;
 
+  int totalPomodoros = 0;
+
   void onTick(Timer timer) {
-    setState(() {
-      totalSeconds -= 1;
-    });
+    if (totalSeconds == 0) {
+      setState(() {
+        isRunning = false;
+        totalSeconds = tF;
+        totalPomodoros += 1;
+      });
+      timer.cancel();
+    } else {
+      setState(() {
+        totalSeconds -= 1;
+      });
+    }
   }
 
   void onStartPressed() {
@@ -40,6 +52,18 @@ class _HomeScreenState extends State<HomeScreen> {
     });
   }
 
+  String format(int seconds) {
+    var duration = Duration(seconds: seconds);
+    print(duration.toString().split(".").first.substring(2, 6));
+    //Duration(seconds: seconds) -> ex) 0:25:00.000000 -> 25:00으로 분리하고싶음
+    //duration(변수)를 스트링으로 바꾼 뒤 split "."기준으로 실행 -> 0:25:00 과 000000 가 분리되어 list에 저장됨
+    //-> .first를 해서 첫번째 아이템만 가져옴
+    //substring(start, end) -> start 부터 end 까지 해당하는 인덱스 스트링 컷팅
+    //인덱스는 2~6 이지만 마지막 인덱스 +1 해서 커트해야
+
+    return duration.toString().split(".").first.substring(2, 7);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -51,7 +75,7 @@ class _HomeScreenState extends State<HomeScreen> {
           child: Container(
             alignment: Alignment.bottomCenter,
             child: Text(
-              '$totalSeconds',
+              format(totalSeconds),
               style: TextStyle(
                 color: Theme.of(context).cardColor,
                 fontSize: 89,
@@ -88,7 +112,7 @@ class _HomeScreenState extends State<HomeScreen> {
                       mainAxisAlignment: MainAxisAlignment.center,
                       children: [
                         Text(
-                          'Pomodors',
+                          'Pomodoros',
                           style: TextStyle(
                             fontSize: 33,
                             color:
@@ -97,7 +121,7 @@ class _HomeScreenState extends State<HomeScreen> {
                           ),
                         ),
                         Text(
-                          '0',
+                          '$totalPomodoros',
                           style: TextStyle(
                             fontSize: 67,
                             color:
