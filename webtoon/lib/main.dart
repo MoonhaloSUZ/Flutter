af58e7e3837cd639be938b4fe721c3636406c57f
MoonhaloSUZ <vvdbal@naver.com>
2023-03-28 21:56:41 +0900

    webtoon api start

--- webtoon/lib/main.dart
+++ webtoon/lib/main.dart
@@ -1,29 +1,25 @@
 import 'package:flutter/material.dart';
-import 'package:webtoon/screen/home_screen.dart';
+import 'package:webtoon/service/api_service.dart';
+import 'package:webtoon/webtoon_screen/home_screen.dart';
+
 //LifeCycle : initState -> build -> ##위젯이 사라질 땐 dispose가 실행됨
 
 void main() {
   runApp(const App());
+
+  ApiService().getTodaysToons();
 }
 
 class App extends StatelessWidget {
   const App({super.key});
+  //이 위젯의 key를 statelessWidget이라는 super class 에 보내는 것
+  //위젯은 ID같은 식별자 역할을 하는 key가 있다.
+  //Flutter가 위젯을 빨리 찾을 수 있도록(식별할 수 있도록) 도와줌.
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSwatch(
-          backgroundColor: const Color(0xFFE7626C),
-        ),
-        textTheme: const TextTheme(
-          displayLarge: TextStyle(
-            color: Color(0xFF232B55),
-          ),
-        ),
-        cardColor: const Color(0xFFF4EDDB),
-      ),
-      home: const HomeScreen(),
+    return const MaterialApp(
+      home: HomeScreen(),
     );
   }
 }
