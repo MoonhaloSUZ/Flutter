7db526546f28966f786827741859a1bfdd9761aa
MoonhaloSUZ <vvdbal@naver.com>
2023-03-28 21:52:23 +0900

    뽀모도로

---
+++ webtoon/lib/pomodoros.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:webtoon/screen/home_screen.dart';
+//LifeCycle : initState -> build -> ##위젯이 사라질 땐 dispose가 실행됨
+
+void main() {
+  runApp(const App());
+}
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSwatch(
+          backgroundColor: const Color(0xFFE7626C),
+        ),
+        textTheme: const TextTheme(
+          displayLarge: TextStyle(
+            color: Color(0xFF232B55),
+          ),
+        ),
+        cardColor: const Color(0xFFF4EDDB),
+      ),
+      home: const HomeScreen(),
+    );
+  }
+}
