5bdd4633c0db948d4dad06a2e4d37c4faf787e56
MoonhaloSUZ <vvdbal@naver.com>
2023-04-03 00:19:50 +0900

    데이터 fetch -> state에 인입

--- webtoon/lib/webtoon_screen/home_screen.dart
+++ webtoon/lib/webtoon_screen/home_screen.dart
@@ -1,7 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:webtoon/model/webtoon_model.dart';
+import 'package:webtoon/service/api_service.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  List<WebtoonModel> webtoons = [];
+  bool isLoading = true;
+
+  void waitForWebToons() async {
+    webtoons = await ApiService.getTodaysToons();
+    isLoading = false;
+    setState(() {});
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    waitForWebToons();
+  }
 
   @override
   Widget build(BuildContext context) {
