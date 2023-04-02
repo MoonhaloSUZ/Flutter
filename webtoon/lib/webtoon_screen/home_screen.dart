e2a9ba61772e59f23324fcf1c781dd7996f6f5a6
MoonhaloSUZ <vvdbal@naver.com>
2023-04-03 00:31:13 +0900

    FutureBuilder

--- webtoon/lib/webtoon_screen/home_screen.dart
+++ webtoon/lib/webtoon_screen/home_screen.dart
@@ -2,29 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:webtoon/model/webtoon_model.dart';
 import 'package:webtoon/service/api_service.dart';
 
-class HomeScreen extends StatefulWidget {
-  const HomeScreen({super.key});
+class HomeScreen extends StatelessWidget {
+  HomeScreen({super.key});
 
-  @override
-  State<HomeScreen> createState() => _HomeScreenState();
-}
-
-class _HomeScreenState extends State<HomeScreen> {
-  List<WebtoonModel> webtoons = [];
-  bool isLoading = true;
-
-  void waitForWebToons() async {
-    webtoons = await ApiService.getTodaysToons();
-    isLoading = false;
-    setState(() {});
-  }
-
-  @override
-  void initState() {
-    // TODO: implement initState
-    super.initState();
-    waitForWebToons();
-  }
+  Future<List<WebtoonModel>> webtoons = ApiService.getTodaysToons();
 
   @override
   Widget build(BuildContext context) {
@@ -42,6 +23,19 @@ class _HomeScreenState extends State<HomeScreen> {
         backgroundColor: Colors.green,
         foregroundColor: Colors.white,
       ),
+      body: FutureBuilder(
+        future: webtoons,
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            return const Text('there is data');
+          }
+          return const Text('loading...');
+        },
+      ),
+      //statefull 위젯을 사용할 필요 없음(await, setstate, isloading등 사용할 필요 x)
+      //FutureBuilder : 기다릴 future랑 builder 함수를 받음
+      //Builder : context와 snapshot을 받음
+      //snapshot : Future의 상태
     );
     //Scaffold : screen을 위한 기본적인 레이아웃과 설정을 제공
   }
