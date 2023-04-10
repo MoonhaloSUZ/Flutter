26a8fe273638afd50d4208b9e4b17cb5c1aa68ca
MoonhaloSUZ <vvdbal@naver.com>
2023-04-10 21:14:04 +0900

    Hero Widget

--- webtoon/lib/screen/detail_screen.dart
+++ webtoon/lib/screen/detail_screen.dart
@@ -34,20 +34,23 @@ class DetailScreen extends StatelessWidget {
           Row(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Container(
-                width: 300,
-                clipBehavior: Clip.hardEdge,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(15),
-                  boxShadow: [
-                    BoxShadow(
-                      blurRadius: 5,
-                      offset: const Offset(10, 10),
-                      color: Colors.black.withOpacity(0.3),
-                    )
-                  ],
+              Hero(
+                tag: id,
+                child: Container(
+                  width: 300,
+                  clipBehavior: Clip.hardEdge,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(15),
+                    boxShadow: [
+                      BoxShadow(
+                        blurRadius: 5,
+                        offset: const Offset(10, 10),
+                        color: Colors.black.withOpacity(0.3),
+                      )
+                    ],
+                  ),
+                  child: Image.network(thumb),
                 ),
-                child: Image.network(thumb),
               ),
             ],
           ),
