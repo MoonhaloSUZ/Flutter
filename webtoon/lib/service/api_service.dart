472d6716404fcb445af3beaaffb1c4740ae5f56b
MoonhaloSUZ <vvdbal@naver.com>
2023-04-11 22:46:46 +0900

    ById method 추가

--- webtoon/lib/service/api_service.dart
+++ webtoon/lib/service/api_service.dart
@@ -1,13 +1,16 @@
 import 'dart:convert';
 
 import 'package:http/http.dart' as http;
+import 'package:webtoon/model/webtoon_detail_model.dart';
+import 'package:webtoon/model/webtoon_episode_model.dart';
 import 'package:webtoon/model/webtoon_model.dart';
 
 class ApiService {
-  final String baseUrl = 'https://webtoon-crawler.nomadcoders.workers.dev';
-  final String today = 'today';
+  static const String today = 'today';
+  static const String baseUrl =
+      'https://webtoon-crawler.nomadcoders.workers.dev';
 
-  Future<List<WebtoonModel>> getTodaysToons() async {
+  static Future<List<WebtoonModel>> getTodaysToons() async {
     List<WebtoonModel> webtoonInstances = [];
     final url = Uri.parse('$baseUrl/$today');
     final response = await http.get(url);
@@ -20,8 +23,35 @@ class ApiService {
       return webtoonInstances;
     }
     throw Error();
+  }
 
-    /*
+  static Future<WebtoonDetailModel> getToonById(String id) async {
+    final url = Uri.parse("$baseUrl/$id");
+    final response = await http.get(url);
+    if (response.statusCode == 200) {
+      final webtoon = jsonDecode(response.body);
+      WebtoonDetailModel.fromJson(webtoon);
+      return WebtoonDetailModel.fromJson(webtoon);
+    }
+    throw Error();
+  }
+
+  static Future<List<WebtoonEpisodeModel>> getLatestEpisodesById(
+      String id) async {
+    List<WebtoonEpisodeModel> episodesInstances = [];
+    final url = Uri.parse("$baseUrl/$id/episodes");
+    final response = await http.get(url);
+    if (response.statusCode == 200) {
+      final episodes = jsonDecode(response.body);
+      for (var episode in episodes) {
+        episodesInstances.add(WebtoonEpisodeModel.fromJson(episode));
+      }
+      return episodesInstances;
+    }
+    throw Error();
+  }
+
+  /*
     ------------------------API Service Memo------------------------
     Future : 미래에 받을 값의 타입을 알려줌
     getTodaysToons() 함수를 호출하면, dart는 api가 응답할 때까지 기달림 == 비동기
@@ -37,5 +67,4 @@ class ApiService {
 
     완료됐을 때, response를 반환할 것이라는 걸 알 수 있음
     */
-  }
 }
